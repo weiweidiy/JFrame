da118d39318795c3ef015f0cfc2b76df63194479
jichunwei <49595272@qq.com>
2023-08-05 00:57:20 +0800

    for mac

---
+++ JFrame/JFrameMSTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using NSubstitute;
+
+namespace JFrameMSTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestRegistAdManager()
+        {
+        }
+    }
+}
+
+
+
