452ecb43f99db4c219a9b46e81bd5b6cba896317
jichunwei <49595272@qq.com>
2023-08-05 01:07:30 +0800

    for mac

---
+++ JFrame/JFrameMSTest/TestAdsManager.cs
@@ -0,0 +1,46 @@
+﻿using NSubstitute;
+using JFrame.Advertisement;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JFrameMSTest
+{
+    [TestClass]
+    public class TestAdsManager
+    {
+
+
+        [TestInitialize]
+        public void Initialize()
+        {
+
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+
+        }
+
+        /// <summary>
+        /// 测试注册一个广告对象，
+        /// </summary>
+        [TestMethod]
+
+        public void TestRegistAdItem()
+        {
+            //Arrange
+            var manager = new AdsManager();
+            var adItem = Substitute.For<IAdItem>();
+
+            //Act
+            manager.RegisterAdItem(adItem);
+
+
+            //Assert
+            Assert.AreEqual(1, manager.GetAdItemsCount());
+        }
+    }
+}
+
+
+
