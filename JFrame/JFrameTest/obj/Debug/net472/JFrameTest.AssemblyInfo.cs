191be4383e55587a5a990559f4542a95b26ed6ac
Jichunwei <49595272@qq.com>
2024-01-22 20:14:03 +0800

    update utility

--- JFrame/JFrameTest/obj/Debug/net472/JFrameTest.AssemblyInfo.cs
+++ JFrame/JFrameTest/obj/Debug/net472/JFrameTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JFrameTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c3af5df071b5c3a10718fa61551cfdf11004914")]
 [assembly: System.Reflection.AssemblyProductAttribute("JFrameTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JFrameTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
