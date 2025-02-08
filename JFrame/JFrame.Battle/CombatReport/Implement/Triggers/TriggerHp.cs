f4d18bdae79f531be0f418b0260e6f18d1acdb6a
Jichunwei <49595272@qq.com>
2025-02-08 10:15:34 +0800

    update

---
+++ JFrame/JFrame.Battle/CombatReport/Implement/Triggers/TriggerHp.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System;
+using JFrame.Common;
+
+namespace JFrame
+{
+    /// <summary>
+    /// 根据HP的情况来触发 type=3 参数：0 攻击距离 1 查找数量 2：队伍id
+    /// </summary>
+    public class TriggerHp : TriggerRange
+    {
+        protected Utility utility = new Utility();
+
+        public override void Update(BattleFrame frame)
+        {
+            //敌人的
+            base.Update(frame);
+
+            //自己的update
+        }
+
+        protected override void SortList(CombatUnit[] arr, float myXPosition)
+        {
+            //arr是敌人数组 按血量排序
+            utility.BinarySort<CombatUnit>(arr, new Compare(myXPosition));
+
+        }
+
+        class Compare : IComparer<CombatUnit>
+        {
+            float myX;
+            public Compare(float myX)
+            {
+                this.myX = myX;
+            }
+
+            int IComparer<CombatUnit>.Compare(CombatUnit x, CombatUnit y)
+            {
+                var unit1 = x;
+                var unit2 = y;
+
+                var unit1Hp = (long)unit1.GetAttributeCurValue(PVPAttribute.HP);
+                var unit2Hp = (long)unit2.GetAttributeCurValue(PVPAttribute.HP);
+
+                if (unit1Hp > unit2Hp)
+                    return -1;
+
+                if (unit1Hp < unit2Hp)
+                    return 1;
+
+                return 0;
+            }
+        }
+
+    }
+}
--- JFrame/JFrame.Battle/obj/Debug/netstandard2.0/JFrame.Battle.AssemblyInfo.cs
+++ JFrame/JFrame.Battle/obj/Debug/netstandard2.0/JFrame.Battle.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JFrame.Battle")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ea552f36419c6cd616fdef43ba016414b6e7eab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("JFrame.Battle")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JFrame.Battle")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
